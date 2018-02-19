a46ccf54e7cdc6f82cafbdeccdfc490c67d6f4a7
Patrick Humphries <patrick.v.humphries@icloud.com>
2018-02-19 10:15:09 -0800

    Add files via upload
    Homework 3:  Option 2:  PyPoll

---
+++ PyPoll/election_factory.py
@@ -0,0 +1,156 @@
+"""
+Module:  election_factory.py
+Author:  Patrick Humphries
+Organization:  USC Viterbi Analytics Boot Camp, January 30, 2018
+Description:  Provide an election model which provides aggregated values from
+    multiple csv files.
+Assumptions:
+    1. Input files will be regular CSV files.
+    2. First row will be a header.
+    3. Columns are Voter ID, County, Candidate
+Methods:
+    get_election_model(file_names)
+"""
+import csv
+
+def get_election_model(file_names):
+    """
+    Module:  election_factory
+    Function:  get_election_model
+    Parameter:  File name of a CSV file with election data.
+    Return:  Election Model, type dict, containing aggregated election data.
+    """
+    # Define Eelection Model
+    # {
+    #   "candidates": candidates,
+    #   "winner": winner
+    # }
+    election_model = {}
+    election_model["number_of_candidates"] = 0
+    election_model["total_votes"] = 0
+
+    # Each candidate in candidates contains aggregated information for that candidate.
+    # {
+    #   "name": candidate
+    # }
+    candidates = {}
+    election_model["candidates"] = candidates
+
+    # Each candidate has attributes.
+    # {
+    #   "name": name
+    #   "percentage": percentage (aggregated)
+    #   "votes": votes (aggregated)
+    # }
+
+    # Process each csv file found in the list argument.
+    for file_name in file_names:
+        try:
+            # Process the csv file.
+            with open(file_name, 'r', encoding='ascii') as csv_file:
+
+                # Create a reader for the CSV file.
+                csv_reader = csv.reader(csv_file, delimiter=',')
+
+                process_rows(csv_reader, election_model)
+
+        except FileNotFoundError as error:
+            print('\nIn election_model.py a ' \
+                    'FileNotFoundError was thrown for file {}.'.format(file_name))
+            print('FileNotFoundError.args:')
+            for arg in error.args:
+                print('\t', arg)
+            print('\n')
+            exit(1)
+
+        except IOError as error:
+            print('\nIn election_model.py a ' \
+                    'IOError was thrown for file {}.'.format(file_name))
+            print('FileNotFoundError.args:')
+            for arg in error.args:
+                print('\t', arg)
+            print('\n')
+            exit(1)
+
+    # Sort and aggregate candidates.
+    sort_and_aggregate_candidates(election_model)
+
+    # Return budgt model containing aggregated amounts.
+    return election_model
+
+def process_rows(csv_reader, election_model):
+    """
+    For each row in the csv_reader, either add or update the candidate in the election model.
+    The candidate key will be used for processing the election model in alphbetical order.
+    """
+    # Metadata
+    voter_id_column = 0
+    name_column = 2
+
+    # i = 0   # Unit Tesing
+
+    for row in csv_reader:
+
+        # Unit Testing
+        # i = i + 1
+        # if i > 4:
+        #     break
+
+        voter_id_string = str(row[voter_id_column])
+
+        # Bypass header.
+        if voter_id_string == "Voter ID":
+            continue
+    
+        # Get candidate's name.
+        name = row[name_column]
+
+        # If the candidate already exists, add the vote to the candidate's votes.
+        # If the candidate does not exist, add the candidate with their vote.
+        candidates = election_model["candidates"]
+
+        if name in candidates:
+            candidate = candidates[name]
+            candidate["votes"] = candidate["votes"] + 1
+        else:
+            candidate = {}
+            candidate["percentage"] = 0
+            candidate["votes"] = 1
+            candidates[name] = candidate
+
+        election_model["total_votes"] = election_model["total_votes"] + 1
+
+    
+def sort_and_aggregate_candidates(election_model):
+    """
+    Calculate aggregations by processing the election model in chronological order.
+    """
+    # Count the number of candidates
+    candidates = election_model["candidates"]      
+    number_of_candidates = len(candidates)    
+    election_model["number_of_candidates"] = number_of_candidates
+
+    # Initialize the winner.
+    winner_name = "nobody"
+    winner_votes = 0
+
+    # Retrieve sort keys for election model and sort them into chronological order.
+    names = candidates.keys()
+    name_list = list(names)
+    name_list.sort()
+
+    # Calculate aggregations by processing candidates in alphbetic order.
+    for name in name_list:
+
+        candidate = candidates[name]
+
+        votes = candidate["votes"]
+
+        candidate["percentage"] = (votes / election_model["total_votes"]) * 100
+        
+        if votes > winner_votes:
+            winner_votes = votes
+            winner_name = name
+
+    # Write aggregations to the election model.
+    election_model["winner"] = winner_name
---
+++ PyPoll/election_processor.py
@@ -0,0 +1,72 @@
+"""
+Module:  election_processor.py
+Author:  Patrick Humphries
+Orgainization:  USC Viterbi Analytics Bootcamp, 1/30
+Output:  Election Results are written to screen and text file.
+Methods:  run()
+Description:
+    1.  Read the input csv files.
+    2.  Build a election model with aggregated votes by candidate.
+    3.  Print and write aggregates per candidate.
+    4.  Print the name of the winner.
+"""
+import election_factory
+import os
+
+def run():
+    # Select all csv files in the current directory to process.
+    # Assumption:  csv file names start with "election_data_"
+    file_names = []
+    directory_entries = os.listdir()
+    for directory_entry in directory_entries:
+        if directory_entry.startswith("election_data_"):
+            if directory_entry.endswith(".csv"):
+                file_names.append(directory_entry)
+
+    # Get the election model created from the input csv files.
+    election_model = election_factory.get_election_model(file_names)
+
+    # Write election to the screen and text file using the election model as the source.
+    with open("election_results.txt", "w") as report_file:
+    
+        report_line = "\n-------------------------------"
+        print(report_line)
+        report_file.write(report_line)
+
+        report_line = "\nTotal Votes:  {:,.0f}".format(election_model["total_votes"])
+        print(report_line)
+        report_file.write(report_line)
+
+        report_line = "\n-------------------------------"
+        print(report_line)
+        report_file.write(report_line)
+
+        candidates = election_model["candidates"]
+
+        names = candidates.keys()
+        name_list = list(names)
+        name_list.sort()
+
+        for name in name_list:   
+            candidate = candidates[name]
+            cp = candidate["percentage"]
+            ct = candidate["votes"]
+            report_line = "\n{}: {:,.1f}% ({:,.0f})".format(name, cp, ct)
+            print(report_line)
+            report_file.write(report_line)
+
+        report_line = "\n-------------------------------"
+        print(report_line)
+        report_file.write(report_line)
+
+        candidate = election_model["winner"]
+        cw = election_model["winner"]
+        report_line = "\nWinner: {}".format(cw)
+        print(report_line)
+        report_file.write(report_line)
+
+        report_line = "\n-------------------------------"
+        print(report_line)
+        report_file.write(report_line)
+
+        input("Press Enter to continue.")
