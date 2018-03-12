a0e80bf5ac1e43c711151b2602b3abce07963a45
pvhumphr <pvhumphr@usc.edu>
2018-03-12 15:58:45 -0700

    Unit 6 World Weather

---
+++ config.py
@@ -0,0 +1,12 @@
+api_keys = []
+api_keys.append("58ed8ba18463655361304e30941eebf0") # api_01
+api_keys.append("0d7936a5fc874d49b29055bca4c41bb2") # api_02
+api_keys.append("1deae7fe6e0e834d44bbe16cae8ac89f") # api_03
+api_keys.append("04e5591a414a1626962e4ac8e0f42a12") # api_04
+api_keys.append("e3da2c250f9b5b9f3413818f02c277d9") # api_05
+api_keys.append("22de4981cb9bc3797833e34dfac2aa98") # api_06
+
+def get_api_keys():
+    return api_keys
+
+print(type(get_api_keys()))
